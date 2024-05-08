fd0dc34b1b1016f693a7c4699d23fc90ae6c934f
assertchris <assertchris@users.noreply.github.com>
2024-05-08 16:45:29 +0000

    Fix styling

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,6 +2,7 @@
 
 namespace AC\BetterMarkdownEditor\Tests;
 
+use AC\BetterMarkdownEditor\BetterMarkdownEditorServiceProvider;
 use BladeUI\Heroicons\BladeHeroiconsServiceProvider;
 use BladeUI\Icons\BladeIconsServiceProvider;
 use Filament\Actions\ActionsServiceProvider;
@@ -9,11 +10,9 @@ use Filament\Forms\FormsServiceProvider;
 use Filament\Infolists\InfolistsServiceProvider;
 use Filament\Notifications\NotificationsServiceProvider;
 use Filament\Support\SupportServiceProvider;
-use Illuminate\Database\Eloquent\Factories\Factory;
 use Livewire\LivewireServiceProvider;
 use Orchestra\Testbench\TestCase as Orchestra;
 use RyanChandler\BladeCaptureDirective\BladeCaptureDirectiveServiceProvider;
-use AC\BetterMarkdownEditor\BetterMarkdownEditorServiceProvider;
 
 class TestCase extends Orchestra
 {
